09aacf52c536c51e35cb7c975245b04bcb545513
Meshwa Patel <patelmeshwa68@gmail.com>
2023-10-16 09:51:50 +0530

    Addes add and remove note functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,47 +1,8 @@
-// Import node.js core modules
-// Creating/syncing the file with passed data
-const fs = require('fs')
-
-fs.writeFileSync('output.txt', 'Hello, This is someone who\'s writing data to txt file!')
-
-// Append some data to file
-fs.appendFileSync('output.txt', ' Do you know me?')
-
-
 // Import our own files
-const notes = require('./notes.js')
+const notesUtil = require('./notes.js')
 
-const message = notes()
-console.log(message)
-
-// Import npm modules
-const validator = require('validator')
-const chalk = require('chalk')
-
-// Validator package
-console.log(validator.isEmail('abc@gmail.com'))
-console.log(validator.isEmail('Hey'))
-console.log(validator.isURL('https://google.com'))
-console.log(validator.isURL('google.com'))
-
-// Chalk package
-console.log(chalk.green('OK'))
-console.log(chalk.blue.bgYellow.bold('Good day!'))
-console.log(chalk.bold.red('Good day!'))
-console.log(chalk.bold.inverse.red('Inversed styling!'))
-
-// Normal processing of command line arguments
-console.log(process.argv)
-
-// Processing of command line arguments using yargs module
+// Import yards module
 const yargs = require('yargs')
-console.log(yargs.argv)
-// node app.js hi => { _: [ 'hi' ], '$0': 'app.js' }
-// node app.js hi => { _: [ 'hi' ], '$0': 'app.js' }
-// node app.js hi --name="Something" => { _: [ 'hi' ], name: 'Something', '$0': 'app.js' }
-
-// Customize yargs version
-// yargs.version('1.1.0')
 
 // Create add command
 yargs.command({
@@ -60,16 +21,27 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Note added with title: ' + argv.title + ' and body: ' + argv.body)
+        // Add newly created note to fs
+        notesUtil.addNote(argv.title, argv.body)
     }
 });
 
-yargs.parse();
 
 // Create remove command
 yargs.command({
     command: 'remove',
+    showInHelp: true,
+    describe: 'Command to remove the note',
+    builder: {
+        title: {
+            describe: 'Title for note to be removed',
+            type: 'string',
+            demandOption: true
+        }
+    },
     handler: function () {
-        console.log('Removing note!')
+        notesUtil.removeNote(yargs.argv.title)
     }
-})
+});
+
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,6 +1,72 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
 const getNotes = function() {
     return 'These are your notes....'
 }
 
+const addNote = function(title, body) {
+    const notes = loadNotes();
+
+    // Don't add redundant entry
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        // Push new note to already existing notes array
+        notes.push({
+            title: title,
+            body: body
+        })
+    
+        // Save on fs
+        saveNotes(notes)
+        console.log(chalk.green('Note added ...'))
+    } else {
+        console.log(chalk.blue('Note with title ' + title + ' already exists!'))
+    }
+
+}
+
+function saveNotes(notes) {
+    const dataString = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataString);
+}
+
+const removeNote = function(title) {
+    const notes = loadNotes();
+
+    // Filtered notes that doesn't have the same title
+    // These notes are now supposed to be there on the file
+    const filteredNotes = notes.filter(function(note) {
+        return note.title !== title;
+    })
+
+    // Only if note that is to be removed is found, update the notes in fs
+    // Else, just log that note not found
+    if (notes.length === filteredNotes.length) {
+        console.log(chalk.red('No note found with title ' + title))
+    } else {
+        // Save on fs
+        saveNotes(filteredNotes)
+        console.log(chalk.green('Note removed with title ' + title))
+    }
+}
+
+const loadNotes = function() {
+    try {
+        const notesBuffer = fs.readFileSync('notes.json')
+        const notesString = notesBuffer.toString()
+        return JSON.parse(notesString)
+    } catch (e) {
+        return []
+    }
+}
+
 // To export variables
-module.exports = getNotes
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
